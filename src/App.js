012e5cacb274f853e59ecf8f2d66294c18404032
kemila <kemiladabare4@gmail.com>
2024-03-17 03:19:35 +0530

    minor bug fixed

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
             <p>{data.name}</p>
           </div>
           <div className='temperature'>
-            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+            {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
           </div>
           <div className='description'>
           {data.weather ? <p>{data.weather[0].main}</p> : null}
